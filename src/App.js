a96b6bc148c6ae56a8ffff3f27b45cfb13fef66d
Owner <94shinshinhy@gmail.com>
2022-11-29 23:00:43 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  console.log('App() 실행');
-
-  const [num, setNum] = useState(5);
-
-  let sample = [
-    { id: 1, name: '김김김' },
-    { id: 2, name: '신신신' },
-    { id: 3, name: '이이이' },
-    { id: 4, name: '박박박' },
-  ];
-
-  // 다운로드 받음
-  const [users, setUsers] = useState(sample); // 래퍼런스가 변경되어야 동작
+  const [data, setData] = useState(0);
+  const [search, setSearch] = useState(0);
 
   const download = () => {
-    setUsers([...sample, { id: num, name: '최최최' }]);
-    setNum(num + 1);
+    // 통신을 통해 다운로드
+    let downloadData = 5;
+    setData(downloadData);
   };
+
+  // 실행시점
+  // (1) App() 함수가 최초 실행될 때(App() 최초 그려질 때)
+  // (2) 상태 변수가 변경될 때(dependencyList에 등록되어있어야함)
+  // (3) 의존 리스트를 관리할 수 있음
+  useEffect(() => {
+    console.log('useEffect 실행됨');
+    download();
+  }, [search]); // search에 의존하기때문에 search가 변경되면 실행됨
 
   return (
     <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1>
-          {u.id}, {u.name}
-        </h1>
-      ))}
+      <button
+        onClick={() => {
+          setSearch(2);
+        }}
+      >
+        검색
+      </button>
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
